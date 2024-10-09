e06518d91dcdd29ed8d09add1c42ea206cebdae8
Anatolylogan <homenkotolya2@gmail.com>
2024-10-09 23:40:59 +0700

    Добавьте файлы проекта.

---
+++ Skills development Classes and structures/Program.cs
@@ -0,0 +1,18 @@
+﻿user tolya = new user();
+string userName = tolya.name;
+int userAge = tolya.age;
+Console.WriteLine($"Имя:{userName} Возраст:{userAge}");
+
+tolya.name = "Tolya";
+tolya.age = 22;
+tolya.Main();
+class user
+{
+    public string name = "Tolya";
+    public int age = 22;
+ 
+   public void Main()
+  {
+   Console.WriteLine($"Имя:{name} Возраст:{age}");
+  }
+}
