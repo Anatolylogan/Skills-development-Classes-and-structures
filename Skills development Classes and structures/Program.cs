ff332ffee7990ba32bfd7d71a95e3cc5d5384a9d
Anatolylogan <homenkotolya2@gmail.com>
2024-10-11 00:26:01 +0700

    1.Исправил задания , поместил в один класс 2.Модифицировал Class User ,Добавил структуру Address

--- Skills development Classes and structures/Program.cs
+++ Skills development Classes and structures/Program.cs
@@ -1,4 +1,6 @@
-﻿user tolya = new user();
-tolya.name = "Tolya";
-tolya.age = 22;
+﻿User silence = new User();
+User tolya = new User("Tolya", 22);
+silence.Print();
 tolya.Print();
+Address nov = new Address("Novoaltaysk","Topolinaya");
+nov.Print();
--- Skills development Classes and structures/user.cs
+++ Skills development Classes and structures/user.cs
@@ -1,14 +1,12 @@
-﻿class user
+﻿struct Address(string city, string street)
 {
-    public string name = "Undefined";
-    public int age;
-    public void Print()
-    {
-        Console.WriteLine($"Имя: {name} Возраст: {age}");
-    }
-    
+  public void Print() => Console.WriteLine($"Город: {city}  Улица: {street}");
 }
-
-       
-
-
+class User
+{
+ public string name;
+ public int age;
+ public User() { name = "Неизвестно"; age = 1; }
+ public User(string n, int a) { name = n; age = a; }
+ public void Print() => Console.WriteLine($"Имя:{name} Возраст:{age}");
+}
