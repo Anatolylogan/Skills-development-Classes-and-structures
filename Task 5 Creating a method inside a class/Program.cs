8de1385b7a9c2e7725f3fa27b2279e0005417ac5
Anatolylogan <homenkotolya2@gmail.com>
2024-10-15 00:54:48 +0700

    2. Выполнил 5 задание. Добавил в класс User публинчый метод DisplayInfo с локальной переменной  messege c типом string. Вывел информацию о пользователе с помощью метода. В Program.cs создал класс Program в нём использовал статический метод Main c парметром массива строк . Вызвал класс User и присвоил название переменной User1 после присвоил значение переменным Name и Age

---
+++ Task 5 Creating a method inside a class/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Task_5_Creating_a_method_inside_a_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User User1 = new User();
+            User1.Name = "Толя";
+            User1.Age = 22;
+            User1.DisplayInfo();
+        }
+    }
+}
